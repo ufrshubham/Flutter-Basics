340545b029275e3f61f9eb1d2ad4e6f9fd3571d1
Ryuzaki <ufrshubham@gmail.com>
2020-07-07 20:05:42 +0530

    Forms and Validation

---
+++ Forms and Validation/lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Forms and Validation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Forms and Validation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: 50.0,
+          horizontal: 10.0,
+        ),
+        child: _buildForm(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            debugPrint('All validations passed!!!');
+          }
+        },
+        child: Icon(Icons.done),
+      ),
+    );
+  }
+
+  Form _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              validator: (String value) {
+                return null;
+              },
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: 'Mobile',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Username cannot be empty';
+                } else if (value.length < 3) {
+                  return 'Username must be at least 3 characters long.';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Username',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              obscureText: true,
+              controller: _passwordController,
+              validator: (String value) {
+                if (value.isEmpty) {
+                  return 'Password cannot be empty';
+                } else if (value.length < 6) {
+                  return 'Password must be at least 6 characters long.';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              obscureText: true,
+              controller: _confirmPasswordController,
+              validator: (String value) {
+                if (value != _passwordController.value.text) {
+                  return 'Passwords do not match!';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
