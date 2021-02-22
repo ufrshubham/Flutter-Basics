8df218862847bebcfa965b6545a806e304371de2
Ryuzaki <ufrshubham@gmail.com>
2021-02-22 19:22:13 +0530

    Connectivity

---
+++ Connectivity check/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Connectivity check',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Connectivity check'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: Connectivity().onConnectivityChanged,
+          builder: (BuildContext context,
+              AsyncSnapshot<ConnectivityResult> snapshot) {
+            if (snapshot != null &&
+                snapshot.hasData &&
+                snapshot.data != ConnectivityResult.none) {
+              return Text(
+                'Connected',
+                style: TextStyle(fontSize: 30),
+              );
+            } else {
+              return Icon(
+                Icons.wifi_off,
+                size: 50,
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
