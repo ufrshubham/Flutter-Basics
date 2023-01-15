253ccac4fd95f5e139c001b55e1c54c993c4f3e3
Ryuzaki <ufrshubham@gmail.com>
2023-01-15 12:12:24 +0530

    Upgrade packages

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,7 +7,6 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
-import 'package:connectivity_plus_web/connectivity_plus_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
@@ -16,7 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
-  ConnectivityPlusPlugin.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
